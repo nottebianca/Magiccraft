cf5ec826c3abc5195ab32d190b99243dfecbd52d
nottebianca59 <emmkakuz@mail.ru>
2024-05-16 23:22:30 +0300

    Change to Fabric

---
+++ src/main/java/net/bambuki/magiccraft/MagicCraft.java
@@ -0,0 +1,18 @@
+package net.bambuki.magiccraft;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MagicCraft implements ModInitializer {
+
+	public static final String MOD_ID = "magiccraft";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/bambuki/magiccraft/MagicCraftClient.java
@@ -0,0 +1,10 @@
+package net.bambuki.magiccraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MagicCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
